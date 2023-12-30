95bfe28a6c0f955e231528e6fd8926ede9cc2817
erolsenol <erol.senol@outlook.com>
2023-12-30 11:56:40 +0300

    header created

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,2 @@
-import main from"./main.js";
 import "./jquery.js"
-
-console.log("test");
-
-const helloWord = $("div")
-helloWord[0].innerText = `${helloWord[0].innerText} !!!`
+import "./main.js";
