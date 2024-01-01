37de34294c31b611c6e793510ffad90e6b68504c
erolsenol <erol.senol@outlook.com>
2024-01-01 09:50:32 +0300

    slider animation created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -15,4 +15,29 @@ $(document).ready(function () {
 
   $("#side-menu-btn").on("click", sideBardHande);
   $(".bg-blur").on("click", sideBardHande);
+
+  // Slider
+
+  let SlideLength = 25;
+  for (let index = 0; index < SlideLength; index++) {
+    $(".slider-image").append(
+      $(
+        "<img class='slider-image-item image-responsive' src='./src/assets/images/slider-img.jpeg'/>"
+      )
+    );
+  }
+
+  let SlideIndex = 0;
+  function showSlide(next) {
+    SlideIndex += next;
+    if (SlideIndex < 0) SlideIndex = SlideLength - 1;
+    else if (SlideIndex >= SlideLength) SlideIndex = 0;
+    
+    $(".slider-image-item").each((i, slide) => {
+      const slideWidth = slide.clientWidth;
+      slide.style.transform = `translateX(-${SlideIndex * slideWidth}px)`;
+    });
+  }
+  $(".slider-left").on("click", () => showSlide(-1));
+  $(".slider-right").on("click", () => showSlide(1));
 });
